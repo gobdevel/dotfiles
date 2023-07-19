035fa96456cb6d0880f30cd24eb98c032e38cb97
Gobind Prasad <gobindp@apstra.com>
2023-07-19 18:08:28 +0000

    Update scalability dev2 patches

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -55,7 +55,7 @@ return require('packer').startup(function(use)
 	}
 
 	use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-	
+
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
 		requires = {'nvim-lua/plenary.nvim'}
@@ -65,5 +65,6 @@ return require('packer').startup(function(use)
 	use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
 	use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
 	use 'L3MON4D3/LuaSnip' -- Snippets plugin
+	use 'lewis6991/gitsigns.nvim' -- Git sign plugin
 
 end)
